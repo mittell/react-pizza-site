c026b2a4c63e013935499b607ed1d7a01546bdda
Chris Mittell <mittell@hotmail.co.uk>
2022-08-28 16:15:36 +0900

    feat: added Feature component and styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero/Hero';
 import Products from './components/Products/Products';
 import { GlobalStyle } from './globalStyles';
-import { productData } from './components/Products/data';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature/Feature';
 
 function App() {
 	return (
@@ -10,6 +11,8 @@ function App() {
 			<GlobalStyle />
 			<Hero />
 			<Products heading='Choose your favourite' data={productData} />
+			<Feature />
+			<Products heading='Sweet Treats for You' data={productDataTwo} />
 		</Router>
 	);
 }
