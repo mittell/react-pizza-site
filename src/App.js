0a5f70d2df18d8f7e74e060a53ac55661b194c6b
Chris Mittell <mittell@hotmail.co.uk>
2022-08-28 14:46:09 +0900

    feat: setup base Navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/Navbar/Navbar';
+import { GlobalStyle } from './globalStyles';
+
 function App() {
-	return <div></div>;
+	return (
+		<Router>
+			<GlobalStyle />
+			<NavBar />
+		</Router>
+	);
 }
 
 export default App;
